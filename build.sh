89c14c6a04a837b609494120da20ab499cd51786
mr1337357 <mr1337357@gmail.com>
2012-10-18 15:02:05 -0700

    adding generic gcc-like compiler support

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+if [ -z $compiler ]
+then
+  if [ which gcc 2&>/dev/null ]
+  then
+    compiler=gcc
+  else
+    echo "cannot find GCC. Please install GCC and run this script again."
+    echo "Ubuntu users: this can be done by running \"sudo apt-get install gcc\""
+    exit
+  fi
+fi
+export compiler
+
 for level in $(ls -d level*)
 do
   cd $level
